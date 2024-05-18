0e5590137bfabe9d3fad88e423459e5cf6e19758
Nad <n.rabbi@live.com>
2024-05-18 13:28:59 +0100

    cleanup

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -105,7 +105,6 @@ export default function Authenticated({ children }) {
                         </div>
 
                         <div className="mt-3 space-y-1">
-                            <ResponsiveNavLink href={route('profile.edit')}>Profile</ResponsiveNavLink>
                             <ResponsiveNavLink method="post" onClick={() => {
                                 api.logout();
                                 window.location = '/login';
